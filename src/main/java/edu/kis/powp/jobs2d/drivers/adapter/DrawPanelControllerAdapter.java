dd5a060b4c5c5c0f536bc83f7cbf9be232e8a6a9
Hubert Cywka <hcywka@gmail.com>
2024-12-08 10:55:17 +0100

    Draw figure in app's main window

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawPanelControllerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawPanelControllerAdapter.java
@@ -4,15 +4,17 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
-public class DrawPanelControllerAdapter extends DrawPanelController implements Job2dDriver {
+public class DrawPanelControllerAdapter implements Job2dDriver {
 	private int startX = 0, startY = 0;
+	private DrawPanelController controller;
 
 	public DrawPanelControllerAdapter() {
-		super();
+		this.controller = DrawerFeature.getDrawerController();
 	}
 
 	@Override
@@ -27,7 +29,7 @@ public class DrawPanelControllerAdapter extends DrawPanelController implements J
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
-		drawLine(line);
+		this.controller.drawLine(line);
 	}
 
 	@Override

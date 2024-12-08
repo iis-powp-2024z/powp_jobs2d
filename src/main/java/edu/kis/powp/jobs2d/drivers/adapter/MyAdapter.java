d14ad7179f229b1f97b8724647afcdd2a9d66ac6
Dawid <241182@edu.p.lodz.pl>
2024-12-08 11:20:44 +0100

    Poprawa pierwszych bledow

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
@@ -4,6 +4,7 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
@@ -26,8 +27,9 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-
-		drawLine(line);
+		this.setPosition(x, y);
+        DrawerFeature.getDrawerController().drawLine(line);
+		
 	}
 
 	@Override

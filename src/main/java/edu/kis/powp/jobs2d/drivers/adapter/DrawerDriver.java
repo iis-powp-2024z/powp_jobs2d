ea8413b9cada0b8b7223617293d74e2e7a4bf328
BartStud <232123@edu.p.lodz.pl>
2024-12-11 20:36:52 +0100

    MyAdapter renamed to DrawerDriver

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerDriver.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerDriver.java
@@ -9,10 +9,10 @@ import edu.kis.powp.jobs2d.features.DrawerFeature;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class DrawerDriver extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
-	public MyAdapter() {
+	public DrawerDriver() {
 		super();
 	}
 
@@ -37,6 +37,6 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 
 	@Override
 	public String toString() {
-		return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+		return "Drawer Driver";
 	}
 }

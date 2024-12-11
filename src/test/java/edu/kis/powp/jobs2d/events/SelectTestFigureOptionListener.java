49c2c4f610de97d04df7b60472601cd980d8f6f6
BartStud <232123@edu.p.lodz.pl>
2024-12-11 20:38:18 +0100

    Added test option for figureScript2

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -2,6 +2,7 @@ package edu.kis.powp.jobs2d.events;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Objects;
 
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
@@ -16,6 +17,10 @@ public class SelectTestFigureOptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		if (Objects.equals(e.getActionCommand(), "Figure Joe 1")) {
+			FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		} else if (Objects.equals(e.getActionCommand(), "Figure Joe 2")){
+			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+		}
 	}
 }

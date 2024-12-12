843daa1921955563fcae724b18d173c1955e2f4c
BeastRacid <polskiarrow@protonmail.com>
2024-12-12 03:51:41 +0100

    Commented out another unnecessary import

--- src/main/java/edu/kis/powp/jobs2d/features/DrawerFeature.java
+++ src/main/java/edu/kis/powp/jobs2d/features/DrawerFeature.java
@@ -3,7 +3,7 @@ package edu.kis.powp.jobs2d.features;
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.events.SelectClearPanelOptionListener;
 
-import javax.swing.JFrame;
+// import javax.swing.JFrame;
 
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 

eb0f7843d587fa3a7df328008269b9d43f332d34
Joanna Ohradka <241231@edu.p.lodz.pl>
2024-12-15 03:31:43 +0100

    DrawerDriver: MyAdapter rename and refactor (3.2 task 1)

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerDriverAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerDriverAdapter.java
@@ -4,36 +4,36 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
-import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
- * driver adapter to drawer with several bugs.
+ * DrawerDriverAdapter is an adapter for Job2dDriver that draws lines when operating.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
-	private int startX = 0, startY = 0;
+public class DrawerDriverAdapter implements Job2dDriver {
+	private int x = 0, y = 0;
+	private DrawPanelController drawerController;
 
-	public MyAdapter() {
-		super();
+	public DrawerDriverAdapter(DrawPanelController drawerController) {
+		this.drawerController = drawerController;
 	}
 
 	@Override
 	public void setPosition(int x, int y) {
-		this.startX = x;
-		this.startY = y;
+		this.x = x;
+		this.y = y;
 	}
 
 	@Override
 	public void operateTo(int x, int y) {
 		ILine line = LineFactory.getBasicLine();
-		line.setStartCoordinates(this.startX, this.startY);
+		line.setStartCoordinates(this.x, this.y);
 		line.setEndCoordinates(x, y);
 		setPosition(x, y);
 
-		DrawerFeature.getDrawerController().drawLine(line);
+		drawerController.drawLine(line);
 	}
 
 	@Override
 	public String toString() {
-		return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+		return "Drawer driver";
 	}
 }
--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -5,10 +5,8 @@ import java.awt.event.ActionEvent;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
-import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
-import edu.kis.powp.jobs2d.drivers.adapter.MyAdapter;
+import edu.kis.powp.jobs2d.drivers.adapter.DrawerDriverAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
@@ -39,22 +37,10 @@ public class TestJobs2dPatterns {
 		DriverFeature.addDriver("Logger Driver", loggerDriver);
 		DriverFeature.getDriverManager().setCurrentDriver(loggerDriver);
 
-		Job2dDriver testDriver = new MyAdapter();
-		DriverFeature.addDriver("Buggy Simulator", testDriver);
+		Job2dDriver drawerDriver = new DrawerDriverAdapter(DrawerFeature.getDrawerController());
+		DriverFeature.addDriver("Drawer Driver", drawerDriver);
 
 		DriverFeature.updateDriverInfo();
-	}
-
-	/**
-	 * Auxiliary routines to enable using Buggy Simulator.
-	 * 
-	 * @param application Application context.
-	 */
-	private static void setupDefaultDrawerVisibilityManagement(Application application) {
-		DefaultDrawerFrame defaultDrawerWindow = DefaultDrawerFrame.getDefaultDrawerFrame();
-		application.addComponentMenuElementWithCheckBox(DrawPanelController.class, "Default Drawer Visibility",
-				new SelectChangeVisibleOptionListener(defaultDrawerWindow), true);
-		defaultDrawerWindow.setVisible(true);
 	}
 
 	/**
